555209a8812134502bc961dc0f14da13cfc45cab
Aditya Tile <adityatile99@gmail.com>
2024-06-22 14:24:28 +0530

    complete testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Client from "@/components/Client";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -10,12 +11,11 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
+        <Client />
       </div>
     </main>
   );
