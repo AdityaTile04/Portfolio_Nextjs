d0560f6b040795462fe93bcdd407a175a04e0022
Aditya Tile <adityatile99@gmail.com>
2025-03-24 12:18:42 +0530

    add experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 
 const Experience = () => {
   return (
-    <div className="py-20 w-full">
+    <div className="py-20 w-full flex flex-col items-center justify-center">
       <h1 className="heading">
         My <span className="text-purple">work experience</span>
       </h1>
 
-      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+      <div className="mt-12 flex flex-col items-center justify-center w-full">
         {workExperience.map((card) => (
           <Button
             key={card.id}
@@ -20,24 +20,23 @@ const Experience = () => {
               background: "rgb(4,7,29)",
               backgroundColor:
                 "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-
               borderRadius: `calc(1.75rem* 0.96)`,
             }}
-            className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            className="text-black dark:text-white border-neutral-200 dark:border-slate-800 w-[800px] max-w-4xl h-auto min-h-[400px] p-10"
           >
-            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+            <div className="flex lg:flex-row flex-col lg:items-center gap-6">
               <Image
                 src={card.thumbnail}
                 alt={card.title}
-                className="lg:w-32 md:w-20 w-16"
-                width={128}
-                height={128}
+                className="lg:w-40 md:w-32 w-24"
+                width={160}
+                height={160}
               />
               <div className="lg:ms-5">
-                <h1 className="text-start text-xl md:text-2xl font-bold">
+                <h1 className="text-start text-2xl md:text-3xl font-bold">
                   {card.title}
                 </h1>
-                <p className="text-start text-white-100 mt-3 font-semibold">
+                <p className="text-start text-white-100 mt-5 font-semibold">
                   {card.desc}
                 </p>
               </div>
