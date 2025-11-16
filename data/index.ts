de915e6d3f92f3fc531b1ac0ee68b0b0796de468
Aditya Tile <adityatile99@gmail.com>
2025-11-16 12:09:21 +0530

    update footer component

--- data/index.ts
+++ data/index.ts
@@ -191,40 +191,22 @@ export const workExperience = [
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
-  // {
-  //   id: 2,
-  //   title: "Mobile App Dev - JSM Tech",
-  //   desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-  //   className: "md:col-span-2", // change to md:col-span-2
-  //   thumbnail: "/exp2.svg",
-  // },
-  // {
-  //   id: 3,
-  //   title: "Freelance App Dev Project",
-  //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-  //   className: "md:col-span-2", // change to md:col-span-2
-  //   thumbnail: "/exp3.svg",
-  // },
-  // {
-  //   id: 4,
-  //   title: "Lead Frontend Developer",
-  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
-  //   className: "md:col-span-2",
-  //   thumbnail: "/exp4.svg",
-  // },
 ];
 
 export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    url: "https://github.com/AdityaTile04",
   },
   {
     id: 2,
     img: "/twit.svg",
+    url: "https://x.com/AdityaTile8",
   },
   {
     id: 3,
     img: "/link.svg",
+    url: "https://www.linkedin.com/in/connectwithadityatile/",
   },
 ];
